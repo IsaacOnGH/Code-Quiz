929a9cb4cd03f687382929ff0a0c9dcd96e99a4b
IsaacOnGH <isaac.melanson@yahoo.com>
2023-08-20 13:53:25 -0500

    Changing index.html to remove clutter of too many divs

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+document.getElementById("start").addEventListener("click", function(){
+  // start the game
+    startGame();
+});
+
+function startGame(){
+  // start the game
+    console.log("Game started");
+    getElementById("code-content").innerHTML = "Game started";
+};
